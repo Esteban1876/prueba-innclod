a6e3a2887f13e6a9565288bc6c0147e5f62bb893
Esteban Arevalo <juan.lopez@mantum.com.co>
2024-05-21 00:14:34 -0500

    terminacion del crud

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use App\Http\Controllers\DocumentoController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-
-
 Route::get('/', function () {
     return view('auth.login');
 });
